286452b84df422abe78300ac96e97cf87a3acb3d
Timo Santi <timo.santi@santicon.fi>
2023-01-24 11:25:13 +0200

    generated lib

---
+++ packages/some-lib/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'some-lib',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/packages/some-lib',
+};
---
+++ packages/some-lib/src/lib/some-lib.ts
@@ -0,0 +1,5 @@
+import path from 'path';
+
+export function someLib(): string {
+  return 'some-lib';
+}
